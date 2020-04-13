73cd1d885e79069208cae4cd8a80cf269e4edd89
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-04-13 19:53:11 +0200

    Cancel Order Compensating Transaction

--- src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -24,6 +24,7 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
     private final Action<BeerOrderStatusEnum, BeerOrderEventEnum>  allocateOrderAction;
     private final Action<BeerOrderStatusEnum, BeerOrderEventEnum>  validationFailureAction;
     private final Action<BeerOrderStatusEnum, BeerOrderEventEnum>  allocationFailureAction;
+    private final Action<BeerOrderStatusEnum, BeerOrderEventEnum>  deAllocationFailureAction;
 
     @Override
     public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
@@ -80,10 +81,7 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
            .and().withExternal()
                 .source(BeerOrderStatusEnum.ALLOCATED).target(BeerOrderStatusEnum.CANCELLED)
                 .event(BeerOrderEventEnum.CANCEL_ORDER)
-//                .action()
+                .action(deAllocationFailureAction)
         ;
-
-
-
     }
 }
